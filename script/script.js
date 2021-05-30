0465ab174b3657d5b47e1ec753fd9ef9b9adf533
Ranjeet Singh <44586579+jeetech77@users.noreply.github.com>
2021-05-31 00:08:36 +0530

    Feat :  Clear button

--- script/script.js
+++ script/script.js
@@ -1,6 +1,8 @@
 const billAmountInput = document.querySelector(".bill-amount");
 const cashAmountInput = document.querySelector(".cash-amount");
 const calculateBtn = document.querySelector(".btn");
+const clearBtn=document.querySelector(".clear-btn");
+const alertText=document.querySelector(".alert-text");
 const twoThouNote=document.querySelector(".two-thou-note");
 const fiveHunNote = document.querySelector(".five-hun-note");
 const hunNote = document.querySelector(".hun-note");
@@ -19,11 +21,15 @@ billAmountInput.addEventListener('focusout',()=>{
 })
 calculateBtn.addEventListener("click", () => {
     var billAmount = Number(billAmountInput.value);
-    if(billAmount!=="" || billAmount!==undefined){
-        cashAmountInput.style.visibility="visible";
-    }
     var cashAmount = Number(cashAmountInput.value);
+    if (billAmount>cashAmount) {
+        alertText.innerText=`Your bill is more than the cash you are providing.`;
+        alertText.style.backgroundColor="red";
+        exit();
+    }
     leftAmount = cashAmount - billAmount;
+    alertText.innerText=`Your left amout is ${leftAmount}`;
+    alertText.style.backgroundColor="green";
     changeAmount.map((note,index) => {
         numOfNotes = leftAmount / note;
         numOfNotes = Math.floor(numOfNotes)
@@ -31,3 +37,12 @@ calculateBtn.addEventListener("click", () => {
         listOfNotes[index].append(`${numOfNotes}`);
     });
 });
+clearBtn.addEventListener('click',()=>{
+    billAmountInput.value='';
+    cashAmountInput.value='';
+    alertText.innerHTML="";
+    alertText.style.backgroundColor = "white";
+    listOfNotes.map(value=>{
+        value.innerHTML="";
+    })
+})
