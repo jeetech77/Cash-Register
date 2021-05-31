a1c2f28147bf3c1d14e6b1094f401109db0d98e0
Ranjeet Singh <44586579+jeetech77@users.noreply.github.com>
2021-05-31 09:35:28 +0530

    Done : Completed
    Completed the 1st project of neogcamp/build.

--- script/script.js
+++ script/script.js
@@ -1,3 +1,5 @@
+ // Selecting elements of html
+
 const billAmountInput = document.querySelector(".bill-amount");
 const cashAmountInput = document.querySelector(".cash-amount");
 const calculateBtn = document.querySelector(".btn");
@@ -10,39 +12,56 @@ const twenNote=document.querySelector(".twen-note");
 const tenNote=document.querySelector(".ten-note");
 const fiveNote=document.querySelector(".five-note");
 const oneNote=document.querySelector(".one-note");
+
 var listOfNotes=[twoThouNote,fiveHunNote,hunNote,twenNote,tenNote,fiveNote,oneNote]
 var changeAmount = [2000, 500, 100, 20, 10, 5, 1]
+
+// To show anoter input field
+
 billAmountInput.addEventListener('focusout',()=>{
     var billAmount = Number(billAmountInput.value);
-    console.log(billAmount);
     if (billAmount !== 0) {
         cashAmountInput.style.visibility = "visible";
     }
 })
+
+// Logic processing here
+
 calculateBtn.addEventListener("click", () => {
     var billAmount = Number(billAmountInput.value);
     var cashAmount = Number(cashAmountInput.value);
-    if (billAmount>cashAmount) {
-        alertText.innerText=`Your bill is more than the cash you are providing.`;
-        alertText.style.backgroundColor="red";
-        exit();
-    }
     leftAmount = cashAmount - billAmount;
-    alertText.innerText=`Your left amout is ${leftAmount}`;
+    if (billAmount>cashAmount) {
+        alertText.innerText=`Cash amount is less than bill amount.`;
+        alertText.style.backgroundColor="red";
+        return('');
+    }
+    alertText.innerText=`Left amount : ${leftAmount}`;
     alertText.style.backgroundColor="green";
     changeAmount.map((note,index) => {
         numOfNotes = leftAmount / note;
         numOfNotes = Math.floor(numOfNotes)
         leftAmount = leftAmount - (numOfNotes * note);
-        listOfNotes[index].append(`${numOfNotes}`);
+        listOfNotes[index].innerText=numOfNotes;
+        if (numOfNotes>0) {
+            listOfNotes[index].style.color="green";
+            listOfNotes[index].style.fontWeight="bolder";
+        }
     });
 });
-clearBtn.addEventListener('click',()=>{
+
+// Checking valid amounts
+
+billAmountInput.addEventListener('focusin',clearFields)
+function clearFields(){
     billAmountInput.value='';
     cashAmountInput.value='';
     alertText.innerHTML="";
-    alertText.style.backgroundColor = "white";
+    cashAmountInput.style.visibility = "hidden";
+    alertText.style.backgroundColor = "inherit";
     listOfNotes.map(value=>{
-        value.innerHTML="";
+        value.innerHTML="0";
+        value.style.color="inherit";
+        value.style.fontWeight="inherit";
     })
-})
+}
