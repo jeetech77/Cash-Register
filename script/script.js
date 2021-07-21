1212d7eac6b6fae913431830f82c19c7f2b7a950
Ranjeet Singh <epiceye77@gmail.com>
2021-07-21 12:15:17 +0530

    Finished : design and conditions

--- script/script.js
+++ script/script.js
@@ -1,25 +1,17 @@
- // Selecting elements of html
+// Selecting elements of document
 
 const billAmountInput = document.querySelector(".bill-amount");
 const cashAmountInput = document.querySelector(".cash-amount");
+const notesContainer = document.querySelector(".flex-container");
 const calculateBtn = document.querySelector(".btn");
-const clearBtn=document.querySelector(".clear-btn");
-const alertText=document.querySelector(".alert-text");
-const twoThouNote=document.querySelector(".two-thou-note");
-const fiveHunNote = document.querySelector(".five-hun-note");
-const hunNote = document.querySelector(".hun-note");
-const twenNote=document.querySelector(".twen-note");
-const tenNote=document.querySelector(".ten-note");
-const fiveNote=document.querySelector(".five-note");
-const oneNote=document.querySelector(".one-note");
+const alertText = document.querySelector(".alert-text");
+const listOfNotes = document.querySelectorAll(".notes")
+let amoutOfNotes = [2000, 500, 100, 20, 10, 5, 1]
 
-var listOfNotes=[twoThouNote,fiveHunNote,hunNote,twenNote,tenNote,fiveNote,oneNote]
-var changeAmount = [2000, 500, 100, 20, 10, 5, 1]
+// To show Cash amount input field
 
-// To show anoter input field
-
-billAmountInput.addEventListener('focusout',()=>{
-    var billAmount = Number(billAmountInput.value);
+billAmountInput.addEventListener('focusout', () => {
+    let billAmount = Number(billAmountInput.value);
     if (billAmount !== 0) {
         cashAmountInput.style.visibility = "visible";
     }
@@ -28,40 +20,57 @@ billAmountInput.addEventListener('focusout',()=>{
 // Logic processing here
 
 calculateBtn.addEventListener("click", () => {
-    var billAmount = Number(billAmountInput.value);
-    var cashAmount = Number(cashAmountInput.value);
-    leftAmount = cashAmount - billAmount;
-    if (billAmount>cashAmount) {
-        alertText.innerText=`Cash amount is less than bill amount.`;
-        alertText.style.backgroundColor="red";
-        return('');
+    let billAmount = Number(billAmountInput.value);
+    let cashAmount = Number(cashAmountInput.value);
+    //  Checking for no values
+    if (billAmount === 0 || cashAmount === 0) {
+        alertText.innerText = `Uppps, Enter some amount please.`;
+        alertText.style.backgroundColor = "red";
+        return; // Exiting the function here.
     }
-    alertText.innerText=`Left amount : ${leftAmount}`;
-    alertText.style.backgroundColor="green";
-    changeAmount.map((note,index) => {
+    // Checking for less values
+    if (billAmount > cashAmount) {
+        alertText.innerText = `Cash amount is less than bill amount.`;
+        alertText.style.backgroundColor = "red";
+        return; // Exiting the function here.
+    }
+    // Checking for equal amouts
+    if (billAmount === cashAmount) {
+        alertText.innerText = `No Amount to be returned.`;
+        alertText.style.backgroundColor = "green";
+        return; // Exiting the function here.
+    }
+
+    leftAmount = cashAmount - billAmount;
+    notesContainer.style.visibility = "visible";
+    alertText.innerText = `Left amount : ${leftAmount}`;
+    alertText.style.backgroundColor = "green";
+    amoutOfNotes.map((note, index) => {
         numOfNotes = leftAmount / note;
         numOfNotes = Math.floor(numOfNotes)
         leftAmount = leftAmount - (numOfNotes * note);
-        listOfNotes[index].innerText=numOfNotes;
-        if (numOfNotes>0) {
-            listOfNotes[index].style.color="green";
-            listOfNotes[index].style.fontWeight="bolder";
+        if (numOfNotes > 0) {
+            listOfNotes[index].innerText = `${amoutOfNotes[index]} Notes : ${numOfNotes}`;
+            listOfNotes[index].style.color = "green";
+            listOfNotes[index].style.fontWeight = "bolder";
         }
     });
 });
 
 // Checking valid amounts
 
-billAmountInput.addEventListener('focusin',clearFields)
-function clearFields(){
-    billAmountInput.value='';
-    cashAmountInput.value='';
-    alertText.innerHTML="";
+billAmountInput.addEventListener('focusin', clearFields);
+cashAmountInput.addEventListener('focusin', clearNotesField)
+
+function clearFields() {
+    billAmountInput.value = '';
+    cashAmountInput.value = '';
     cashAmountInput.style.visibility = "hidden";
+    clearNotesField();
+}
+
+function clearNotesField() {
+    alertText.innerHTML = "";
     alertText.style.backgroundColor = "inherit";
-    listOfNotes.map(value=>{
-        value.innerHTML="0";
-        value.style.color="inherit";
-        value.style.fontWeight="inherit";
-    })
+    notesContainer.style.visibility = "hidden";
 }
